3ff1e101d9a33f095d8092bf986f6106584a8ed7
Katanych <k4t4ny4@mail.ru>
2021-10-31 04:58:51 +0700

    calc v1

--- crypto_calc.py
+++ crypto_calc.py
@@ -1,4 +1,5 @@
 '''Место для импортирования модулей'''
+from data import *
 
 
 class Crypto_calc(object):
@@ -19,50 +20,100 @@ class Crypto_calc(object):
         self.num_tokens = num_tokens
         self.token2 = token2
 
-    def price_p2p(self):
-        '''Цена peer-to-peer сделки для продающего'''
+    def total_order_price(self):
+        '''Оптимальная общая стоимость сделки'''
 
-        pass
+        cost_cex_sale = self.sell_to_CEX()
+        cost_cex_buy = self.ideal_price("CEX")
 
-    def discount_amount(self):
-        '''Размер скидки для покупателя'''
+        return cost_cex_sale + ((cost_cex_buy - cost_cex_sale) / 2)
         
-        pass
+    def optimal_discount(self):
+        '''Оптимальный размер скидки для покупателя
+        
+        Полученное значение опирается на оптимальную стоимость
+        сделки и ту сумму, за которую в идеальном случае мы могли
+        бы продать эти токены.
+        
+        '''
+
+        cost_disc = self.ideal_price("CEX") - self.total_order_price()
+        return (cost_disc * 100) / self.ideal_price("CEX")
 
     def price_token(self):
         '''Цена токена'''
 
         pass
-
-    def get_converted_price(self):
-        '''Функция возвращает стоимость новых токенов на CEX
+    
+    def ideal_price(self, type_stock):
+        '''Функция идеальной цены, если бы мы продали по курсу.
         
-        Функция возвращает общую стоимость токенов, на которую был
-        соврешен обмен (token2)
+        Определяем максимальную возможную цену, исходя из
+        курса (средней цены продаж) на бирже.
+        
+        '''
+
+        max_price = 0
+        for stock in TOKEN[type_stock]:
+            if float(TOKEN[type_stock][stock]["price"]) > max_price:
+                max_price = float(TOKEN[type_stock][stock]["price"])
+        
+        return float(max_price) * self.num_tokens
+
+    def sell_to_CEX(self):
+        '''Конечная цена при потери в 40% на CEX.
+        
+        Здесь будет описан алгоритм получения сконвертированной цены
+        для CEXs
+
+        '''
+
+        total_sum = 0.
+        for stock in TOKEN["CEX"]:
+            total_sum += float(TOKEN["CEX"][stock]["volume"])
+        
+        result_cost = 0
+        for stock in TOKEN["CEX"]:
+            coef_prior = (float(TOKEN["CEX"][stock]["volume"])) / total_sum
+            result_cost += coef_prior * self.ideal_price("CEX")
+        
+        # Уменьшаем на 40%
+        result_cost = (result_cost * 60) / 100.
+
+        return result_cost
+    
+    def buy_to_CEX(self):
+        '''Конечная цена при переплате на 40%'''
+
+        total_sum = 0.
+        for stock in TOKEN["CEX"]:
+            total_sum += float(TOKEN["CEX"][stock]["volume"])
+        
+        result_cost = 0
+        for stock in TOKEN["CEX"]:
+            coef_prior = (float(TOKEN["CEX"][stock]["volume"])) / total_sum
+            result_cost += coef_prior * self.ideal_price("CEX")
+        
+        # Уменьшаем на 40%
+        result_cost = (result_cost * 140) / 100.
+
+        return result_cost
+        
+    def sell_to_DEX(self):
+        '''Конечная цена при продаже на DEX.
+        
+        Определяется с помощью готового инструмента.
         
         '''
 
         pass
-
-    def convert_price(self, stocks=['Binance'], ):
-        '''Функция возвращает стоимость токенов, на который обменяли.
-        
-        Количество валют, которые мы получим при продаже
-        набора токенов на нескольких централизованных биржах CEX 
-        (stocks) за один вечер.
-        
-        '''
-
-        sum = 0
-        for stock in stocks:
-            # Суммируем стоимость, полученную с каждой биржи 
-            sum += self.__calc_end_cost(stock)
-            
-        return sum
+    
+    def buy_to_DEX(self):
+        pass
 
     def __calc_end_cost(self, stock):
         '''Функция подсчитывает конечную цену проданных токенов на бирже'''
-        
+         
         pass
 
     def __num_tokens_to_sale(self, stock):
--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from calculate_discount import *
 from crypto_calc import Crypto_calc
 
 
 TOKEN = "BAT"           # Токен, которые планируется продать
 NUM_TOKENS = 5000000    # 5 млн токенов к продаже
-TOKEN_TO_EXCH = "BTC"   # Токен, на который будем менять
+TOKEN_TO_EXCH = "USD"   # Токен, на который будем менять
 
 def main():
     calc = Crypto_calc(TOKEN, NUM_TOKENS, TOKEN_TO_EXCH)
-    discont = calc.discount_amount()
-    end_price_p2p = calc.price_p2p()
+    discont = calc.optimal_discount()
+    end_price_p2p = calc.total_order_price()
     end_price_token = calc.price_token()
     print("Размер скидки для покупателя:", discont, \
         "\nКонечная цена peer-to-peer сделки:", end_price_p2p, \
             "\nКонечная цена токена:", end_price_token)
+    #print(calc.optimal_discount())
 
 if __name__ == '__main__':
     main()
